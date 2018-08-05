0085f58c7b8c8610fca4d9cac55a84814623f1e4
nihique <kovarik.martin@gmail.com>
2018-08-05 14:57:11 +0200

    excercise 1.1. finished

--- ex-1-1.rkt
+++ ex-1-1.rkt
@@ -19,19 +19,39 @@
 (interpreted(/ 6 2))
 (expected 3)
 
-;;; (+ (* 2 4) (- 4 6))
-;;; (define a 3)
-;;; (define b (+ a 1))
-;;; (+ a b (* a b))
-;;; (= a b)
-;;; (if (and (> b a) (< b (* a b)))
-;;;     b
-;;;     a)
-;;; (cond ((= a 4) 6)
-;;;       ((= b 4) (+ 6 7 a))
-;;;       (else 25))
-;;; (+ 2 (if (> b a) b a))
-;;; (* (cond ((> a b) a)
-;;;          ((< a b) b)
-;;;          (else -1))
-;;;    (+ a 1))
+(interpreted (+ (* 2 4) (- 4 6)))
+(expected 6)
+
+(define a 3)
+(interpreted a)
+(expected 3)
+
+(define b (+ a 1))
+(interpreted b)
+(expected 4)
+
+(interpreted (+ a b (* a b)))
+(expected 19)
+
+(interpreted(= a b))
+(expected #f)
+
+(interpreted (if (and (> b a) (< b (* a b)))
+                b
+                a))
+(expected 4)
+
+(interpreted (cond 
+              ((= a 4) 6)
+              ((= b 4) (+ 6 7 a))
+              (else 25)))
+(expected 16)
+
+(interpreted (+ 2 (if (> b a) b a)))
+(expected 6)
+
+(interpreted (* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1)))
+(expected 16)
